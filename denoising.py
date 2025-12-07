03a116589a0eb001e8c891df37b3fe2504860945
Rohit-R-Rao <rohitrrao2021@gmail.com>
2025-12-07 00:53:03 -0800

    Final Commit: Completed EEG denoising and evaluation with 2 best models

--- denoising.py
+++ denoising.py
@@ -1,14 +1,8 @@
-# Copyright (c) 2022, Kwanhyung Lee. All rights reserved.
-#
-# Licensed under the MIT License; 
-# you may not use this file except in compliance with the License.
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+"""
+Author: Rohit R Rao
+Date: 2025-12-07
+Description: This file contains the functions for denoising the EEG signal.
+"""
 import numpy as np
 import torch
 import torch.nn as nn
